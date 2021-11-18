1e1d8316a87bd460e8bb54ff380d4c51fbdd9214
wb_songjiangwei <wb_songjiangwei@meituan.com>
2021-11-18 11:42:20 +0800

    添加reset.css

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,12 @@ module.exports = {
   lintOnSave: false,
   configureWebpack: {
     resolve: {
-      extensions: ['.js', '.vue', '.json', '.ts', 'tsx']
-    }
+      modules: ['node_modules'],
+      extensions: ['.js', '.vue', '.jsx', '.tsx', '.json', '.css', '.scss'], // 按顺序解析这些后缀名
+      alias: {
+        '@': resolve('src/components'),
+        '@styles': resolve('src/styles')
+      }
+    },
   }
 }
